12e93dbf256a04ff74bceb9214765d0d48f31f0f
Steve Behm <stevtbehm1@gmail.com>
2022-02-01 13:09:13 -0800

    changed nesting and seperated order data from item data for javascript-data-model exercise.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,93 +1,92 @@
 var orderHistory = [
   {
-    orderOneItems: [
+    placed: 'August 4, 2020',
+    total: 34.00,
+    shipsTo: 'JS Masher',
+    orderNum: 114 - 3941689 - 8772232,
+    dod: 'Aug 8, 2020',
+    reviewProd: true,
+    buyAgain: true,
+    viewItem: true,
+    prodSupport: false,
+    items: [
       {
-        placed: 'August 4, 2020',
-        total: 34.00,
-        shipsTo: 'JS Masher',
-        orderNum: 114 - 3941689 - 8772232,
-        dod: 'Aug 8, 2020',
+        type: 'book',
         title: 'JavaScript for impatient rogrammers',
         author: 'Rauschmayer, Dr. Axel',
         returnDue: 'Sep 7, 2020',
         cost: 31.55,
-        reviewProd: true,
-        buyAgain: true,
-        viewItem: true,
-        prodSupport: false
+        image: ''
       }
     ]
   },
   {
-    orderTwoItems: [
+    placed: 'July 19, 2020',
+    total: 44.53,
+    shipsTo: 'JS Masher',
+    orderNum: 113 - 9984268 - 1280257,
+    dod: 'Jul 20, 2020',
+    reviewProd: true,
+    buyAgain: true,
+    viewItem: true,
+    prodSupport: false,
+    items: [
       {
-        placed: 'July 19, 2020',
-        total: 44.53,
-        shipsTo: 'JS Masher',
-        orderNum: 113 - 9984268 - 1280257,
-        dod: 'Jul 20, 2020',
+        type: 'book',
         title: 'The Timeless Way of Building',
         author: 'Alexander, Christopher',
         returnDue: 'Aug 19, 2020',
         cost: 41.33,
-        reviewProd: true,
-        buyAgain: true,
-        viewItem: true,
-        prodSupport: false
+        image: ''
       }
     ]
   },
   {
-    orderThreeItems: [
+    placed: 'July 4, 2020',
+    total: 17.22,
+    shipsTo: 'JS Masher',
+    orderNum: 114 - 2875557 - 9059409,
+    dod: 'Jul 7, 2020',
+    reviewProd: true,
+    buyAgain: true,
+    viewItem: true,
+    prodSupport: false,
+    items: [
       {
-        placed: 'July 4, 2020',
-        total: 17.22,
-        shipsTo: 'JS Masher',
-        orderNum: 114 - 2875557 - 9059409,
-        dod: 'Jul 7, 2020',
+        type: 'hardware',
         title: 'Gamecube Controller Adapter, Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
         returnDue: 'Aug 5, 2020',
         cost: 15.98,
-        reviewProd: true,
-        buyAgain: true,
-        viewItem: true,
-        prodSupport: false
+        image: ''
       }
     ]
   },
   {
-    orderFourItems: [
+    placed: 'July 3, 2020',
+    total: 138.93,
+    shipsTo: 'JS Masher',
+    orderNum: 113 - 2883177 - 2648248,
+    dod: 'Jul 5, 2020',
+    reviewProd: true,
+    buyAgain: true,
+    viewItem: true,
+    prodSupport: false,
+    items: [
       {
-        placed: 'July 3, 2020',
-        total: 138.93,
-        shipsTo: 'JS Masher',
-        orderNum: 113 - 2883177 - 2648248,
-        dod: 'Jul 5, 2020',
+        type: 'hardware',
         title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
         returnDue: 'Aug 4, 2020',
         cost: 94.95,
-        reviewProd: true,
-        buyAgain: true,
-        viewItem: true,
-        prodSupport: false
+        image: ''
       },
       {
-        placed: 'July 3, 2020',
-        total: 138.93,
-        shipsTo: 'JS Masher',
-        orderNum: 113 - 2883177 - 2648248,
-        dod: 'Jul 5, 2020',
+        type: 'book',
         title: 'The Art of Sql',
         author: 'Faroult, Stephane',
         returnDue: 'Aug 4, 2020',
         cost: 33.99,
-        reviewProd: true,
-        buyAgain: true,
-        viewItem: true,
-        prodSupport: false
+        image: ''
       }
     ]
   }
 ];
-
-console.log('vale of cost of the second item in the last order:', orderHistory[3].orderFourItems[1].cost);
