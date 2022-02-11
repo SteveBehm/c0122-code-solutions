8c80aab4a2403e27d26812d34e30ec8013719923
Steve Behm <stevtbehm1@gmail.com>
2022-02-11 13:28:33 -0800

    completed javascript-and-json exercise.

---
+++ javascript-and-json/main.js
@@ -0,0 +1,31 @@
+var libraryArr = [
+  {
+    isbn: '0001',
+    title: 'Artemis',
+    author: 'Andy Weir'
+  },
+  {
+    isbn: '0002',
+    title: 'The Martian',
+    author: 'Andrew Weir'
+  },
+  {
+    isbn: '0003',
+    title: 'Project Hail Mary',
+    author: 'Andrew Weird'
+  }
+];
+console.log('my library of books:', libraryArr);
+console.log('type of library:', typeof libraryArr);
+
+JSON.stringify(libraryArr);
+console.log('JSON library of books:', JSON.stringify(libraryArr));
+console.log('type of JSON library:', typeof JSON.stringify(libraryArr));
+
+var student = '{"Number id": 3731, "String name": "Steve"}';
+console.log('JSON string:', student);
+console.log('JSON string type of:', typeof studentJSON);
+
+var studentObj = JSON.parse(student);
+console.log('student JS object:', studentObj);
+console.log('student JS object type of:', typeof studentObj);
