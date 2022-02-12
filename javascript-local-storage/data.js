5fc4daf2174a5bb6a71e25331c59421dd77739e9
Steve Behm <stevtbehm1@gmail.com>
2022-02-11 17:47:25 -0800

    removed if statement from function.

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -1,12 +1,13 @@
 /* exported todos */
-window.addEventListener('beforeunload', handleBeforeUnload);
 var todos = [];
 var previousTodosJSON = localStorage.getItem('javascript-local-storage');
 
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
+
+window.addEventListener('beforeunload', handleBeforeUnload);
 function handleBeforeUnload(event) {
   var todosJSON = JSON.stringify(todos);
   localStorage.setItem('javascript-local-storage', todosJSON);
-  if (previousTodosJSON !== null) {
-    todos = JSON.parse(previousTodosJSON);
-  }
 }
