b9077cd4efc58d6ae27ec239098cef28e551fc21
Steve Behm <stevtbehm1@gmail.com>
2022-03-11 11:50:29 -0800

    created deck of cards with function.

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -25,217 +25,27 @@ var players = [
   }
 ];
 
-// create a list of card objects
-var cards = [
-  {
-    rank: 'Ace',
-    suit: 'hearts'
-  },
-  {
-    rank: '2',
-    suit: 'hearts'
-  },
-  {
-    rank: '3',
-    suit: 'hearts'
-  },
-  {
-    rank: '4',
-    suit: 'hearts'
-  },
-  {
-    rank: '5',
-    suit: 'hearts'
-  },
-  {
-    rank: '6',
-    suit: 'hearts'
-  },
-  {
-    rank: '7',
-    suit: 'hearts'
-  },
-  {
-    rank: '8',
-    suit: 'hearts'
-  },
-  {
-    rank: '9',
-    suit: 'hearts'
-  },
-  {
-    rank: '10',
-    suit: 'hearts'
-  },
-  {
-    rank: 'Jack',
-    suit: 'hearts'
-  },
-  {
-    rank: 'Queen',
-    suit: 'hearts'
-  },
-  {
-    rank: 'King',
-    suit: 'hearts'
-  },
-  {
-    rank: 'Ace',
-    suit: 'spades'
-  },
-  {
-    rank: '2',
-    suit: 'spades'
-  },
-  {
-    rank: '3',
-    suit: 'spades'
-  },
-  {
-    rank: '4',
-    suit: 'spades'
-  },
-  {
-    rank: '5',
-    suit: 'spades'
-  },
-  {
-    rank: '6',
-    suit: 'spades'
-  },
-  {
-    rank: '7',
-    suit: 'spades'
-  },
-  {
-    rank: '8',
-    suit: 'spades'
-  },
-  {
-    rank: '9',
-    suit: 'spades'
-  },
-  {
-    rank: '10',
-    suit: 'spades'
-  },
-  {
-    rank: 'Jack',
-    suit: 'spades'
-  },
-  {
-    rank: 'Queen',
-    suit: 'spades'
-  },
-  {
-    rank: 'King',
-    suit: 'spades'
-  },
-  {
-    rank: 'Ace',
-    suit: 'diamonds'
-  },
-  {
-    rank: '2',
-    suit: 'diamonds'
-  },
-  {
-    rank: '3',
-    suit: 'diamonds'
-  },
-  {
-    rank: '4',
-    suit: 'diamonds'
-  },
-  {
-    rank: '5',
-    suit: 'diamonds'
-  },
-  {
-    rank: '6',
-    suit: 'diamonds'
-  },
-  {
-    rank: '7',
-    suit: 'diamonds'
-  },
-  {
-    rank: '8',
-    suit: 'diamonds'
-  },
-  {
-    rank: '9',
-    suit: 'diamonds'
-  },
-  {
-    rank: '10',
-    suit: 'diamonds'
-  },
-  {
-    rank: 'Jack',
-    suit: 'diamonds'
-  },
-  {
-    rank: 'Queen',
-    suit: 'diamonds'
-  },
-  {
-    rank: 'King',
-    suit: 'diamonds'
-  },
-  {
-    rank: 'Ace',
-    suit: 'clubs'
-  },
-  {
-    rank: '2',
-    suit: 'clubs'
-  },
-  {
-    rank: '3',
-    suit: 'clubs'
-  },
-  {
-    rank: '4',
-    suit: 'clubs'
-  },
-  {
-    rank: '5',
-    suit: 'clubs'
-  },
-  {
-    rank: '6',
-    suit: 'clubs'
-  },
-  {
-    rank: '7',
-    suit: 'clubs'
-  },
-  {
-    rank: '8',
-    suit: 'clubs'
-  },
-  {
-    rank: '9',
-    suit: 'clubs'
-  },
-  {
-    rank: '10',
-    suit: 'clubs'
-  },
-  {
-    rank: 'Jack',
-    suit: 'clubs'
-  },
-  {
-    rank: 'Queen',
-    suit: 'clubs'
-  },
-  {
-    rank: 'King',
-    suit: 'clubs'
+// Create a deck of cards
+var cardValues = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+var suits = ['hearts', 'spades', 'clubs', 'diamonds'];
+
+function createDeck(values, type) {
+  var deck = [];
+  var obj = {};
+
+  for (var i = 0; i < values.length; i++) {
+    for (var j = 0; j < type.length; j++) {
+      obj.rank = values[i];
+      obj.suit = type[j];
+      deck.push(obj);
+      obj = {};
+    }
   }
-];
+  console.log('deck:', deck);
+  return deck;
+}
+var cards = createDeck(cardValues, suits);
+console.log('cards:', cards);
 
 /*
 Shuffle the cards using the Fisher-Yates Method
