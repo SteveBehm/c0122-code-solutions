c85ca70ef4c60ca4cba3f138a63a67de56badce0
Steve Behm <stevtbehm1@gmail.com>
2022-03-11 10:59:53 -0800

    completed javascript-typing-tutor.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,10 +1,17 @@
 var $letters = document.querySelectorAll('span');
-window.addEventListener('keydown', handleKeyDown);
 
 var index = 0;
 function handleKeyDown(event) {
-  if (event.key === $letters[index].textContent) {
+  if (event.key === ' ' && event.key === $letters[index].textContent) {
+    $letters[index].className = '';
+    $letters[index + 1].className = 'normal';
     index++;
+  } else if (event.key === $letters[index].textContent) {
+    $letters[index].className = 'correct';
+    $letters[index + 1].className = 'normal';
+    index++;
+  } else if (event.key !== $letters[index].textContent) {
+    $letters[index].className = 'incorrect';
   }
-
 }
+window.addEventListener('keydown', handleKeyDown);
