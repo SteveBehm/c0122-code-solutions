1ba001c6dc8883522fbfb29b82276d0ddee39ff7
Steve Behm <stevtbehm1@gmail.com>
2022-03-16 13:19:28 -0700

    completed node-require exercise.

---
+++ node-require/multiply.js
@@ -0,0 +1,6 @@
+function product(num1, num2) {
+  const multiply = num1 * num2;
+  return multiply;
+}
+
+module.exports = product;
---
+++ node-require/subtract.js
@@ -0,0 +1,6 @@
+function difference(num1, num2) {
+  const diff = num1 - num2;
+  return diff;
+}
+
+module.exports = difference;
