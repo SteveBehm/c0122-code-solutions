437fa56ec20dfc64b17ec5bccf9d7b30d9125af5
Steve Behm <stevtbehm1@gmail.com>
2022-03-21 20:13:48 +0000

    finished feature allowing to view note by id.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const appObj = express();
 const dataJSON = require('./data.json');
+const notesObj = dataJSON.notes;
 
 appObj.listen(3000, () => {
   // eslint-disable-next-line no-console
@@ -17,4 +18,23 @@ appObj.get('/api/notes', (req, res) => {
   }
 
   res.json(gradeArr);
+});
+
+appObj.get('/api/notes/:id', (req, res) => {
+  const targetNoteId = req.params.id;
+
+  if (!Number.isInteger(parseInt(targetNoteId)) || parseInt(targetNoteId) <= 0) {
+    const errObj = {
+      error: 'id must be a positive integer'
+    };
+    res.status(400).json(errObj);
+
+  } else if (notesObj[targetNoteId] === undefined) {
+    const noId = {
+      error: `cannot find note with id ${targetNoteId}`
+    };
+    res.status(404).json(noId);
+  } else {
+    res.json(notesObj[targetNoteId]);
+  }
 });
