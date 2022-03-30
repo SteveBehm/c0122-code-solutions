25a1c64d35a56c92e962afbcac0c89fc8c5c0231
Steve Behm <stevtbehm1@gmail.com>
2022-03-30 21:54:16 +0000

    completed react-events-and-state.

---
+++ react-events-and-state/src/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CustomButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ isClicked: true });
+  }
+
+  render() {
+    if (this.state.isClicked === true) {
+      return (
+        <button onClick={this.handleClick}>Thanks!</button>
+      );
+    } else {
+      return (
+        <button onClick={this.handleClick}>{this.props.text}</button>
+      );
+    }
+  }
+}
+
+const element = (
+  <CustomButton text="Click Me!" />
+);
+
+ReactDOM.render(
+  element,
+  document.querySelector('#root')
+);
