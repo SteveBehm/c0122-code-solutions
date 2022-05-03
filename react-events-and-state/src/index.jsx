987cefd7e7203961ccdab81e041daa435e28fe23
Steve Behm <stevtbehm1@gmail.com>
2022-05-03 00:29:36 +0000

    converted code to hooks and added some functionality to the button.

--- react-events-and-state/src/index.jsx
+++ react-events-and-state/src/index.jsx
@@ -1,56 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-class CustomButton extends React.Component {
-  // FIRST instantiate a CustomButton object from this class
-  constructor(props) {
-    super(props);
-    this.state = { isClicked: false };
+function CustomButton() {
+  const [buttonTxt, setText] = useState(() => {
+    return 'Click Me!';
+  });
+  const [clicked, setClick] = useState(() => {
+    return false;
+  });
 
-    this.handleClick = this.handleClick.bind(this);
-    // bind creates a permanent this object
-    // create a bound copy of the prototype method handleClick
-    // assign the bound copy to an "own" property of this object
-  }
-
-  handleClick() {
-    // decide how to update state in event listeners
-    // please transition to a new state
-    this.setState({ isClicked: true });
-    // schedules a re-render with a new state
-  }
-
-  // NEXT React immediately call the render method to get the the React elements
-  render() {
-    // 100% decide on how the UI should look
-    if (this.state.isClicked === true) {
-      return (
-        <button onClick={this.handleClick}>Thanks!</button>
-        /*
-        {
-          type: 'button,
-          props: {
-            children: 'Thanks!'
-          }
-        }
-        */
-      );
+  function handleClick() {
+    if (clicked) {
+      setText('Click Me!');
+      setClick(false);
     } else {
-      return (
-        <button onClick={this.handleClick}>Click Me!</button>
-        /*
-        {
-          type: 'button,
-          props: {
-            onClick: this.handleClick,
-            children: 'Click Me!'
-          }
-        }
-        */
-      );
+      setText('Thank you!');
+      setClick(true);
     }
   }
+  return (
+    <button onClick={handleClick}>{buttonTxt}</button>
+  );
 }
+
+// class CustomButton extends React.Component {
+//   // FIRST instantiate a CustomButton object from this class
+//   constructor(props) {
+//     super(props);
+//     this.state = { isClicked: false };
+
+//     this.handleClick = this.handleClick.bind(this);
+//     // bind creates a permanent this object
+//     // create a bound copy of the prototype method handleClick
+//     // assign the bound copy to an "own" property of this object
+//   }
+
+//   handleClick() {
+//     // decide how to update state in event listeners
+//     // please transition to a new state
+//     this.setState({ isClicked: true });
+//     // schedules a re-render with a new state
+//   }
+
+//   // NEXT React immediately call the render method to get the the React elements
+//   render() {
+//     // 100% decide on how the UI should look
+//     if (this.state.isClicked === true) {
+//       return (
+//         <button onClick={this.handleClick}>Thanks!</button>
+//         /*
+//         {
+//           type: 'button,
+//           props: {
+//             children: 'Thanks!'
+//           }
+//         }
+//         */
+//       );
+//     } else {
+//       return (
+//         <button onClick={this.handleClick}>Click Me!</button>
+//         /*
+//         {
+//           type: 'button,
+//           props: {
+//             onClick: this.handleClick,
+//             children: 'Click Me!'
+//           }
+//         }
+//         */
+//       );
+//     }
+//   }
+// }
 
 const element = (
   <CustomButton />
